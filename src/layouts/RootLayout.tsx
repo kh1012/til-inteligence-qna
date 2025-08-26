695ebd0c4596a53eb0cfad4fab26cdbae830128e
kh1012 <kh1012@midasit.com>
2025-08-26 13:55:39 +0900

    RootLayout 초안 작성

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,1 +1,17 @@
 // 공통 (Header/Footer) 레이아웃
+
+import { Outlet } from "react-router-dom";
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
+
+export default function RootLayout() {
+  return (
+    <div className="min-h-dvh flex flex-col">
+      <Header />
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+}
